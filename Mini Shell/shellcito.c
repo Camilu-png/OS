6cb34250983058ba3fac10e6d54e69d0898037bb
Camilu-png <camilachillan99@gmail.com>
2022-07-27 20:39:17 -0400

    init commit

---
+++ Mini Shell/shellcito.c
@@ -0,0 +1,7 @@
+#include "shellcito.h"
+
+int main()
+{
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ Mini Shell/shellcito.h
@@ -0,0 +1,9 @@
+#ifndef SHELLCITO_H
+#define SHELLCITO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#endif
