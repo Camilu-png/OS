45691962a55b660384dea2a6c63606895455222f
Camilu-png <camilachillan99@gmail.com>
2022-07-28 23:31:10 -0400

    feat(shelcito): add background and foreground

--- Mini Shell/shellcito.c
+++ Mini Shell/shellcito.c
@@ -1,7 +1,73 @@
 #include "shellcito.h"
 
+int execute_command(char *command, char *arguments[1024], int argument_count, int background)
+{
+    if (access(command, F_OK) != 0)
+    {
+        printf("%s command not found\n", command);
+        return 1;
+    }
+
+    int pid, status;
+    pid = fork();
+    if (pid == 0)
+    {
+        if (background == 0)
+        {
+            execvp(command, arguments);
+        }
+        else
+        {
+            execvp(command, arguments);
+            wait(&status);
+        }
+    }
+    else
+    {
+        if (background == 0)
+        {
+            wait(&status);
+        }
+    }
+    return 0;
+}
+
 int main()
 {
-    printf("Hello, World!\n");
+
+    while (1)
+    {
+
+        printf("$ ");
+
+        char input[1024];
+        fgets(input, 1024, stdin);
+
+        char *tokens[1024];
+        int token_count = 0;
+        char *token = strtok(input, " ");
+        while (token != NULL)
+        {
+            tokens[token_count] = token;
+            token_count++;
+            token = strtok(NULL, " ");
+        }
+
+        // Remove the newline to the last token
+        tokens[token_count - 1][strlen(tokens[token_count - 1]) - 1] = '\0';
+
+        char *command = tokens[0];
+        char *arguments[1024];
+        arguments[0] = command;
+        int argument_count = 1;
+        int background = tokens[token_count - 1][0] == '&';
+        for (int i = 1; i < token_count - 1 - background; i++)
+        {
+            arguments[argument_count] = tokens[i];
+            argument_count++;
+        }
+
+        int state = execute_command(command, arguments, argument_count, background);
+    }
     return 0;
 }
--- Mini Shell/shellcito.h
+++ Mini Shell/shellcito.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int execute_command(char *command, char *arguments[1024], int argument_count, int background);
 
 #endif
